d22a2c7cd7e8d38bc4eeaf3123e6bcf06f5c5b30
Stephen Martin <mediabytes@icloud.com>
2018-04-22 21:10:43 -0400

    updated root

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
-
-var apiProxy = proxy('/rentals', {
-    target: 'https://www.yougotlistings.com/api/rentals/search.php?key=1cY2iaM5eLEWXp7wmtUvgSPsozCJqQDubZ0BNKnk&include_mls=1',
-    changeOrigin: true   // for vhosted sites
-});
-
+var apiProxy = proxy('/rentals', { target: 'https://www.yougotlistings.com/api/rentals/search.php?key=1cY2iaM5eLEWXp7wmtUvgSPsozCJqQDubZ0BNKnk&include_mls=1', changeOrigin: true } );
 var app = express();
 
 app.use(apiProxy);
+
+app.get('/', (req, res) => {
+	res.send('You are connected to the root level of the Common Realty Group main server.')
+})
 app.listen(process.env.PORT || 3001, ()=> {
 	console.log(`Server is running on port ${process.env.PORT}`)
 });
