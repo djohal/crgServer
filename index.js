4c82290635a93a65a3a50be32ff0531027aed3bc
Stephen Martin <mediabytes@icloud.com>
2018-04-22 21:19:25 -0400

    updated CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var cors = require('cors')
 var proxy = require('http-proxy-middleware');
 var apiProxy = proxy('/rentals', { target: 'https://www.yougotlistings.com/api/rentals/search.php?key=1cY2iaM5eLEWXp7wmtUvgSPsozCJqQDubZ0BNKnk&include_mls=1', changeOrigin: true } );
 var app = express();
 
+app.use(cors())
 app.use(apiProxy);
 
 app.get('/', (req, res) => {
