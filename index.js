a3d9aeb2d34349c7ab5d65b50b046abed59a27cd
Stephen Martin <mediabytes@icloud.com>
2018-05-09 00:57:15 -0400

    Added Zillow Feed

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var cors = require('cors')
-var proxy = require('http-proxy-middleware');
-var listingAPI = proxy('/rentals', { target: 'https://www.yougotlistings.com/api/rentals/search.php?key=1cY2iaM5eLEWXp7wmtUvgSPsozCJqQDubZ0BNKnk&include_mls=1', changeOrigin: true } );
-var officeAPI = proxy('/office', { target: 'https://www.yougotlistings.com/api/accounts/search.php?key=1cY2iaM5eLEWXp7wmtUvgSPsozCJqQDubZ0BNKnk', changeOrigin: true } ); 
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const proxy = require('http-proxy-middleware');
+const listingAPI = proxy('/rentals', { target: 'https://www.yougotlistings.com/api/rentals/search.php?key=1cY2iaM5eLEWXp7wmtUvgSPsozCJqQDubZ0BNKnk&include_mls=1', changeOrigin: true } );
+const officeAPI = proxy('/office', { target: 'https://www.yougotlistings.com/api/accounts/search.php?&key=1cY2iaM5eLEWXp7wmtUvgSPsozCJqQDubZ0BNKnk', changeOrigin: true } );
+const ZillowAPI = proxy('/zillow', { target: 'http://www.yougotlistings.com/feed?id=AKEXjsch69nLvbVGDZ7503Fzl4erR1iYPx8pwJoS&code=0rQ3', changeOrigin: true } );
+const app = express();
+
 
 app.use(cors())
 app.use(listingAPI);
-app.use(officeAPI)
+app.use(officeAPI);
+app.use(ZillowAPI);
 
 app.get('/', (req, res) => {
-	res.send('You are connected to the root level of the main server @CommonRealtyGroup.')
+	res.send('You are connected to the root level')
 })
 app.listen(process.env.PORT || 3001, ()=> {
 	console.log(`Server is running on port ${process.env.PORT}`)
